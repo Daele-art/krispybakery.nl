a66fca78a90c7ab67a490d10fa51f09e567e11c1
Daele-art <dalebout.wesley@gmail.com>
2026-04-23 17:49:46 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,69 +1,50 @@
 import Layout from "./Layout.jsx";
-
 import Home from "./Home";
-
 import Bestellen from "./Bestellen";
-
 import Over from "./Over";
-
 import FAQ from "./FAQ";
-
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 const PAGES = {
-    
-    Home: Home,
-    
-    Bestellen: Bestellen,
-    
-    Over: Over,
-    
-    FAQ: FAQ,
-    
-}
+  Home: Home,
+  Bestellen: Bestellen,
+  Over: Over,
+  FAQ: FAQ,
+};
 
 function _getCurrentPage(url) {
-    if (url.endsWith('/')) {
-        url = url.slice(0, -1);
-    }
-    let urlLastPart = url.split('/').pop();
-    if (urlLastPart.includes('?')) {
-        urlLastPart = urlLastPart.split('?')[0];
-    }
+  if (url.endsWith('/')) {
+    url = url.slice(0, -1);
+  }
 
-    const pageName = Object.keys(PAGES).find(page => page.toLowerCase() === urlLastPart.toLowerCase());
-    return pageName || Object.keys(PAGES)[0];
-}
+  let urlLastPart = url.split('/').pop();
 
-// Create a wrapper component that uses useLocation inside the Router context
-function PagesContent() {
-    const location = useLocation();
-    const currentPage = _getCurrentPage(location.pathname);
-    
-    return (
-        <Layout currentPageName={currentPage}>
-            <Routes>            
-                
-                    <Route path="/" element={<Home />} />
-                
-                
-                <Route path="/Home" element={<Home />} />
-                
-                <Route path="/Bestellen" element={<Bestellen />} />
-                
-                <Route path="/Over" element={<Over />} />
-                
-                <Route path="/FAQ" element={<FAQ />} />
-                
-            </Routes>
-        </Layout>
-    );
+  if (urlLastPart.includes('?')) {
+    urlLastPart = urlLastPart.split('?')[0];
+  }
+
+  const cleanPart = urlLastPart.replace('#', '');
+
+  const pageName = Object.keys(PAGES).find(
+    (page) => page.toLowerCase() === cleanPart.toLowerCase()
+  );
+
+  return pageName || Object.keys(PAGES)[0];
 }
 
 export default function Pages() {
-    return (
-        <Router>
-            <PagesContent />
-        </Router>
-    );
+  const location = useLocation();
+  const currentPage = _getCurrentPage(location.pathname);
+
+  return (
+    <Layout currentPageName={currentPage}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Bestellen" element={<Bestellen />} />
+        <Route path="/Over" element={<Over />} />
+        <Route path="/FAQ" element={<FAQ />} />
+      </Routes>
+    </Layout>
+  );
 }
